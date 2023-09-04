faadb4caec79e63f852e6630479a47cc1d8f024c
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2023-09-04 21:59:46 +0200

    added some icons

--- main.py
+++ main.py
@@ -12,14 +12,50 @@ app_body.iconphoto(False, small_img)
 # TODO find out how to change window taskbar icon
 # app_body.iconbitmap("lock-icon-large.ico")
 
-# Create nav bar scene widget
+
+# Goes back to password generation area
+def new_password_suggestion():
+    return (print("new password"))
+
+
+# Copy password
+def copy_password():
+    return (print("copy password"))
+
+
+# Nav bar area
 nav_frame = tk.Frame(app_body, width=400, height=70)
 nav_frame.config(bg="#5B6565")
 nav_frame.grid(row=0, column=0, padx=0, pady=0)
 
+# Nav bar buttons
 
-# Create main scene widget
+
+# Main scene area
 body_frame = tk.Frame(app_body, width=400, height=530)
+# body_frame.config(bg="#D9D9D9")
 body_frame.config(bg="#D9D9D9")
 body_frame.grid(row=1, column=0, padx=0, pady=0)
+body_frame.grid_propagate(False)
+body_frame.grid_columnconfigure(0, weight=1)
+body_frame.grid_rowconfigure(0, weight=1)
+
+# suggested password
+suggested_password = tk.Label(body_frame, text="new password here")
+suggested_password.grid(row=0, column=0, padx=0, pady=0)
+
+# Copy password button
+copy_img = tk.PhotoImage(file='Improbable/assets/copy-icon.png')
+copy_password_button = tk.Button(body_frame,
+                                 command=copy_password,
+                                 image=copy_img)
+copy_password_button.grid(row=0, column=1)
+
+# Regenerate password
+regenerate_img = tk.PhotoImage(file='Improbable/assets/shuffle-icon.png')
+generate_password_button = tk.Button(body_frame,
+                                     command=new_password_suggestion,
+                                     image=regenerate_img)
+generate_password_button.grid(row=1, column=0)
+
 app_body.mainloop()
